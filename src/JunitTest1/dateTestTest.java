64191059316ed2ad1c8f372ff0d38681726cdc73
csc <csc@DESKTOP-2TPM2TL>
2019-05-09 09:43:19 +0800

    添加了简单的junit

---
+++ src/JunitTest1/dateTestTest.java
@@ -0,0 +1,31 @@
+package JunitTest1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import operation3.AccountValidatorUtil;
+
+class dateTestTest {
+
+	String email1 = "123";
+    String email2 = "cao@163.com";
+    String email3 = "caoshencheng";
+
+
+
+    @Test
+    public void isEmailLegal2() throws Exception {
+        assertTrue(AccountValidatorUtil.isEmail(email2));
+    } 
+    @Test
+    public void isEmailLegal1() throws Exception {
+        assertTrue(AccountValidatorUtil.isEmail(email1));
+    }
+    @Test
+    public void isEmailLegal3() throws Exception {
+        assertTrue(AccountValidatorUtil.isEmail(email3));
+    }
+
+
+}
---
+++ src/operation3/dateTest.java
@@ -0,0 +1,13 @@
+package operation3;
+
+public class dateTest {
+	String a="12345";
+	String b="1998-01-04";
+	
+	public boolean isDate(String str)
+	{
+		return AccountValidatorUtil.isIDCard(str);
+	}
+	
+
+}
